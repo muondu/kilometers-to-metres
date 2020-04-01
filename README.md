4f3aba1c437ae59976478bbd5b1cb85eb1c83dac
muondu <munenemuondu@gmail.com>
2020-04-01 21:04:24 +0300

    I have added a README

---
+++ README.md
@@ -0,0 +1,2 @@
+# My project
+This project ask you to put any amount in km and changes into m
---
+++ pr1.py
@@ -0,0 +1,4 @@
+print("Hello,Today I will help you change km to mm")
+input1 = int(input("Enter your amount in metres:  "))
+result = a*1000
+print(result)
